2f6d54760f83703fc2a66f9c40908e4f60f3d07d
Omadar79 <99696526+Omadar79@users.noreply.github.com>
2026-08-08 10:00:36 -0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using MonoRogue.Core;
+
+public static class Program
+{
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    public static void Main()
+    {
+        using ( var game = new RogueGame() )
+    {
+        game.Run();
+    }
+            
+    }
+}
